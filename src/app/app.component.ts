8303e69ab563759c0faeed9661482df9a5da70db
jasonstrs <56001431+jasonstrs@users.noreply.github.com>
2020-04-16 16:18:34 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ import * as firebase from 'firebase';
 export class AppComponent {
   constructor(){
     var firebaseConfig = {
-      apiKey: "AIzaSyDcXCG4q0NIIAS4mwPw33nWbsUOoBsNwdw",
-      authDomain: "books-37769.firebaseapp.com",
-      databaseURL: "https://books-37769.firebaseio.com",
-      projectId: "books-37769",
-      storageBucket: "books-37769.appspot.com",
-      messagingSenderId: "230004915042",
-      appId: "1:230004915042:web:f5f3ee36c616cf22194a3b"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
